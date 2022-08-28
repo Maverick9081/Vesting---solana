aba4012253bb7c0c43cdad0462eb2d98074edc2e
Maverick9081 <faliyaabhishek@gmail.com>
2022-08-28 18:30:43 +0530

    Addeing a beneficiary and setting up the account

--- programs/Vesting/src/lib.rs
+++ programs/Vesting/src/lib.rs
@@ -1,8 +1,8 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{ self, CloseAccount, Mint, SetAuthority, TokenAccount, Transfer };
-// use spl_token::instruction::AuthorityType;
+use anchor_spl::token::{ self, CloseAccount, Mint, SetAuthority, TokenAccount, Transfer};
+use spl_token::instruction::AuthorityType;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Dw5fH4qu2zegeKKQca876aup8pdUnz84118BW16ASABB");
 
 const VESTING_SEED: &[u8] = b"vesting";
      const VAULT_SEED: &[u8] = b"vault";
@@ -11,9 +11,36 @@ const VESTING_SEED: &[u8] = b"vesting";
 pub mod vesting {
     use super::*;
 
-     
+    pub fn add_beneficiary(ctx: Context<AddBeneficiary>,total_amount : u64,cliff_time : i64,start_time : i64,end_time :i64) -> Result<()> {
+        
+        ctx.accounts.vesting_account.beneficiary = ctx.accounts.beneficiary.to_account_info().key();
+        ctx.accounts.vesting_account.beneficiary_ata = ctx.accounts.beneficiary_ata.to_account_info().key();
+        ctx.accounts.vesting_account.start_time = start_time;
+        ctx.accounts.vesting_account.end_time = end_time;
+        ctx.accounts.vesting_account.cliff_time = cliff_time;
+        // ctx.accounts.vesting_account.duration = end_time - start_time;
+        ctx.accounts.vesting_account.owner = ctx.accounts.owner.to_account_info().key();
+        ctx.accounts.vesting_account.mint = ctx.accounts.mint.to_account_info().key();
+        ctx.accounts.vesting_account.total_vesting_amount = total_amount;
+        ctx.accounts.vesting_account.released_amount = 1;
+        msg!("0");
+        let (vault_authority,_vault_authority_bump) = Pubkey::find_program_address(
+            &[VAULT_SEED],
+            ctx.program_id
+        );
+        msg!("1");
 
-    pub fn add_beneficiary(ctx: Context<AddBeneficiary>) -> Result<()> {
+        token::set_authority(
+            ctx.accounts.into_set_authority_context(),
+            AuthorityType::AccountOwner,
+            Some(vault_authority)
+        )?;
+        msg!("2");
+         token::transfer(
+            ctx.accounts.into_transfer_to_pda_context(),
+            total_amount
+         )?;
+
         Ok(())
     }
 
@@ -29,6 +56,8 @@ pub struct AddBeneficiary<'info> {
     ///CHECK
     #[account(mut,signer)]
     pub owner : AccountInfo<'info>,
+    #[account(mut)]
+    pub owner_ata : Account <'info,TokenAccount>,
      ///CHECK
     pub beneficiary : AccountInfo<'info>,
     pub beneficiary_ata : Account <'info,TokenAccount>,
@@ -53,7 +82,7 @@ pub struct AddBeneficiary<'info> {
             beneficiary.key().as_ref()
         ],
         bump,
-        space = 90
+        space = 176
     )]
     pub vesting_account : Account<'info,VestingAccount>,
      ///CHECK
@@ -66,9 +95,11 @@ pub struct AddBeneficiary<'info> {
 #[account]
 pub struct VestingAccount {
     pub beneficiary: Pubkey,
+    pub beneficiary_ata : Pubkey,
     pub start_time: i64,
+    pub end_time : i64,
     pub cliff_time: i64,
-    pub duration: i64,
+    // pub duration: i64,
     pub owner: Pubkey,
     pub mint: Pubkey,
     pub total_vesting_amount: u64,
@@ -80,4 +111,24 @@ pub enum Roles {
     Advisor,
     Partner,
     Mentor
+}
+
+impl<'info> AddBeneficiary <'info>{
+
+    fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+        let cpi_accounts = SetAuthority {
+            account_or_mint: self.vault_account.to_account_info().clone(),
+            current_authority: self.owner.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+
+    fn into_transfer_to_pda_context(&self) -> CpiContext<'_,'_,'_,'info,Transfer<'info>>{
+        let cpi_accounts = Transfer{
+            from : self.owner_ata.to_account_info().clone(),
+            to : self.vault_account.to_account_info().clone(),
+            authority : self.owner.clone()
+        };
+        CpiContext::new(self.token_program.clone(),cpi_accounts)
+    }
 }
